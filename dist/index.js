ba4b5b3e908d686c7ccb259013cf86c0df0b1c08
ChandlerMorris <150638881+ChandlerMorris@users.noreply.github.com>
2024-06-30 16:14:13 -0400

    Added error handling for getFiles

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,33 @@
-const fs = require('node:fs');
-const getFiles = (dir, _prevFiles) => {
-    _prevFiles = _prevFiles || [];
-    const files = fs.readdirSync(dir);
-    for (const i in files) {
-        let name = `${dir}/${files[i]}`;
-        if (fs.statSync(name).isDirectory()) {
-            getFiles(name, _prevFiles);
-        }
-        else {
-            const path = name.split(/[./]/g);
-            const index = path.indexOf('feature');
-            if (index !== -1) {
-                const filename = path[index - 1];
-                _prevFiles.push(filename);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const getFiles = (dir, _prevFiles = []) => {
+    try {
+        const files = fs_1.default.readdirSync(dir);
+        for (const i in files) {
+            let name = `${dir}/${files[i]}`;
+            if (fs_1.default.statSync(name).isDirectory()) {
+                getFiles(name, _prevFiles);
             }
+            else {
+                const path = name.split(/[./]/g);
+                const index = path.indexOf('feature');
+                if (index !== -1) {
+                    const filename = path[index - 1];
+                    _prevFiles.push(filename);
+                }
+            }
+            ;
         }
         ;
     }
-    ;
+    catch (err) {
+        console.error(`Error reading directory: ${dir}: ${err}`);
+    }
     return _prevFiles;
 };
-console.log(getFiles('./features', []));
+console.log(getFiles('./features'));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,22 +1,25 @@
-const fs = require('node:fs');
+import fs from 'fs';
 
-const getFiles = (dir: string, _prevFiles: string[] | []): string[] => {
-    _prevFiles = _prevFiles || [];
-    const files = fs.readdirSync(dir);
-    for (const i in files) {
-        let name = `${dir}/${files[i]}`;
-        if (fs.statSync(name).isDirectory()) {
-            getFiles(name, _prevFiles);
-        } else {
-            const path = name.split(/[./]/g);
-            const index = path.indexOf('feature');
-            if (index !== -1) {
-                const filename = path[index - 1];
-                _prevFiles.push(filename);
-            }
+const getFiles = (dir: string, _prevFiles: string[] = []): string[] => {
+    try {
+        const files = fs.readdirSync(dir);
+        for (const i in files) {
+            let name = `${dir}/${files[i]}`;
+            if (fs.statSync(name).isDirectory()) {
+                getFiles(name, _prevFiles);
+            } else {
+                const path = name.split(/[./]/g);
+                const index = path.indexOf('feature');
+                if (index !== -1) {
+                    const filename = path[index - 1];
+                    _prevFiles.push(filename);
+                }
+            };
         };
-    };
+    } catch (err) {
+        console.error(`Error reading directory: ${dir}: ${err}`)
+    }
     return _prevFiles;
 };
 
-console.log(getFiles('./features', []));
+console.log(getFiles('./features'));
